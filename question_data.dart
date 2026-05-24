7627092953685cf505dd8446de9eacb56dc48445
nive <nivethanive943@gmail.com>
2026-05-24 10:38:22 +0530

    Add files via upload

---
+++ question_data.dart
@@ -0,0 +1,37 @@
+List<Map<String, dynamic>> questionsData = [
+  {
+    "question": "What is Dart mainly used for?",
+    "options": [
+      "Web scraping",
+      "Mobile app development",
+      "Game design",
+      "Database management",
+    ],
+    "answer": "Mobile app development",
+  },
+  {
+    "question": "Which framework uses Dart as its primary language?",
+    "options": ["React Native", "Angular", "Flutter", "Vue"],
+    "answer": "Flutter",
+  },
+  {
+    "question": "Which keyword is used to declare a constant value in Dart?",
+    "options": ["let", "final", "var", "const"],
+    "answer": "const",
+  },
+  {
+    "question": "Which widget is used for immutable UI in Flutter?",
+    "options": [
+      "StatefulWidget",
+      "InheritedWidget",
+      "StatelessWidget",
+      "Container",
+    ],
+    "answer": "StatelessWidget",
+  },
+  {
+    "question": "Which function is the entry point of a Dart program?",
+    "options": ["start()", "init()", "main()", "run()"],
+    "answer": "main()",
+  },
+];
